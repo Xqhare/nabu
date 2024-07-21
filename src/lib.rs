5f605d60e5045f10848d2023fb085323fbcf2bd4
Xqhare <xqhare@web.de>
2024-07-21 13:50:12 +0200

    feat: added basic filestructure

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod xff;
+mod xff_value;
+
+pub mod serde {
+    pub fn deserialize() {
+        todo!()
+    }
+
+    pub fn serialize() {
+        todo!()
+    }
 }
