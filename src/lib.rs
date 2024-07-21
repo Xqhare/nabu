ccb2b1ab986a4e1ff3b77ed64e6ea8a46c8630a9
Xqhare <xqhare@web.de>
2024-07-22 00:00:22 +0200

    feat: whatever path is passed in, the extension is now set to xff (for read too)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-mod xff;
 mod error;
+
+pub mod xff;
 
 const XFF_VERSION: u8 = 0;
 
@@ -11,14 +12,17 @@ pub mod serde {
     use crate::XFF_VERSION;
 
     pub fn read<P>(path: P) -> Result<Vec<XffValue>, NabuError> where P: AsRef<std::path::Path> {
-        deserialize_xff(path.as_ref())
+        let path_with_xff_extension = path.as_ref().with_extension("xff");
+        deserialize_xff(&path_with_xff_extension)
     }
 
     pub fn write<P>(path: P, data: Vec<XffValue>) -> Result<(), NabuError> where P: AsRef<std::path::Path> {
-        serialize_xff(path.as_ref(), data, XFF_VERSION)
+        let path_with_xff_extension = path.as_ref().with_extension("xff");
+        serialize_xff(&path_with_xff_extension, data, XFF_VERSION)
     }
 
     pub fn write_legacy<P>(path: P, data: Vec<XffValue>, xff_version: u8) -> Result<(), NabuError> where P: AsRef<std::path::Path> {
-        serialize_xff(path.as_ref(), data, xff_version)
+        let path_with_xff_extension = path.as_ref().with_extension("xff");
+        serialize_xff(&path_with_xff_extension, data, xff_version)
     }
 }
