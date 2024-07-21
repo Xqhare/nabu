cc0dd47800091bbbd1a3c3e8bf6ad6edbddcb61f
Xqhare <xqhare@web.de>
2024-07-21 23:14:03 +0200

    feat: added read, write and write legacy as API endpoints

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,24 @@
 mod xff;
+mod error;
+
+const XFF_VERSION: u8 = 0;
 
 pub mod serde {
-    pub fn read<P: AsRef<std::path::Path>>(path: P) -> std::io::Result() {
-        todo!()
+    use crate::error::NabuError;
+    use crate::xff::deserializer::deserialize_xff;
+    use crate::xff::serializer::serialize_xff;
+    use crate::xff::value::XffValue;
+    use crate::XFF_VERSION;
+
+    pub fn read<P>(path: P) -> Result<Vec<XffValue>, NabuError> where P: AsRef<std::path::Path> {
+        deserialize_xff(path.as_ref())
     }
 
-    pub fn write() {
-        todo!()
+    pub fn write<P>(path: P, data: Vec<XffValue>) -> Result<(), NabuError> where P: AsRef<std::path::Path> {
+        serialize_xff(path.as_ref(), data, XFF_VERSION)
+    }
+
+    pub fn write_legacy<P>(path: P, data: Vec<XffValue>, xff_version: u8) -> Result<(), NabuError> where P: AsRef<std::path::Path> {
+        serialize_xff(path.as_ref(), data, xff_version)
     }
 }
