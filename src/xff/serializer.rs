2962449177beb119a974a082f7342e0dac6356c4
Xqhare <xqhare@web.de>
2024-07-22 20:04:00 +0200

    chore: fixed all cmd chars were escaped and length of data is now little endian encoded

--- src/xff/serializer.rs
+++ src/xff/serializer.rs
@@ -13,11 +13,14 @@ pub fn serialize_xff(path: &Path, data: Vec<XffValue>, ver: u8) -> Result<(), Na
 
 fn serialize_xff_v0(path: &Path, data: Vec<XffValue>) -> Result<(), NabuError>{
     let mut out: Vec<u8> = Default::default();
+    // Only true if the last pushed data was a command character
+    let mut escape_open = false;
     // Version 0
     out.push(0);
     for value in data {
         match value {
             XffValue::String(s) => {
+                escape_open = false;
                 // STX
                 out.push(2);
                 for c in s.chars() {
@@ -27,6 +30,7 @@ fn serialize_xff_v0(path: &Path, data: Vec<XffValue>) -> Result<(), NabuError>{
                 out.push(3);
             },
             XffValue::Number(n) => {
+                escape_open = false;
                 // STX
                 out.push(2);
                 for entry in n.as_u8() {
@@ -36,9 +40,10 @@ fn serialize_xff_v0(path: &Path, data: Vec<XffValue>) -> Result<(), NabuError>{
                 out.push(3);
             },
             XffValue::Data(d) => {
+                escape_open = false;
                 // DLE
                 out.push(16);
-                let len = d.len.to_be_bytes().to_vec();
+                let len = d.len.to_le_bytes().to_vec();
                 let len_bind: Vec<&u8> = len.iter().take(5).collect(); 
                 for entry in len_bind {
                     out.push(*entry);
@@ -50,24 +55,26 @@ fn serialize_xff_v0(path: &Path, data: Vec<XffValue>) -> Result<(), NabuError>{
                 out.push(16);
             },
             XffValue::CommandCharacter(c) => {
+                if escape_open {
+                    // remove ending ESC
+                    out.remove(out.len() - 1);
+                } else {
+                    // put starting ESC
+                    out.push(27);
+                }
                 match c {
                     super::value::CommandCharacter::Escape => {
-                        // ESC
-                        out.push(27);
                         // ESC needs to be ESC escaped
                         out.push(27);
                         out.push(c.as_u8());
-                        // ESC
-                        out.push(27);
                     }
                     _ => {
-                        // ESC
-                        out.push(27);
                         out.push(c.as_u8());
-                        // ESC
-                        out.push(27);
                     }
                 }
+                // ESC
+                out.push(27);
+                escape_open = true;
             },
         }
     }
