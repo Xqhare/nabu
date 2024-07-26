99019ddad68314c75dad238fac4142871d1278a5
Xqhare <xqhare@web.de>
2024-07-26 21:41:49 +0200

    feat: added tests for NabuDB

--- tests/key_value.rs
+++ tests/key_value.rs
@@ -159,8 +159,7 @@ mod core {
         let bin_data3 = std::fs::read("LICENSE").unwrap();
         let bin_data5 = std::fs::read("README.md").unwrap();
         let bin_data6 = std::fs::read("tests/v0.rs").unwrap();
-        let data_store = {
-            vec![
+        let data_store = vec![
                 XffValue::String("Padding".to_string()),
                 XffValue::Data(Data {len: bin_data0.len(), data: bin_data0}),
                 XffValue::Number(Number::from(42.22222E-222)),
@@ -183,8 +182,7 @@ mod core {
                 XffValue::CommandCharacter(CommandCharacter::Escape),
                 XffValue::Data(Data::from(bin_data3)),
                 XffValue::Data(Data::from(bin_data5)),
-            ]
-        };
+            ];
         for (index, entry) in data_store.iter().enumerate() {
             data.insert(format!("key{}", index), entry.clone());
         }
@@ -197,5 +195,139 @@ mod core {
 
         // cleanup
         let _ = std::fs::remove_file(path);
+    }
+}
+
+#[cfg(test)]
+#[cfg(feature = "key_value_store")]
+mod store {
+    use nabu::{key_value_store::new_nabudb, xff::value::{CommandCharacter, Data, Number, XffValue}, features::key_value::store::NabuDB};
+
+    #[test]
+    fn basic() {
+        let mut db: NabuDB = new_nabudb("basic_db.xff").unwrap();
+        let data = vec![
+            XffValue::String("Padding".to_string()),
+            XffValue::Data(Data {len: 10, data: vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]}),
+            XffValue::CommandCharacter(CommandCharacter::UnitSeparator),
+            XffValue::Number(Number::from(42.22222E+222)),
+            XffValue::CommandCharacter(CommandCharacter::SoftHyphen),
+            XffValue::Number(Number::from(-42.22222E222)),
+            XffValue::String("Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.".to_string())
+        ];
+        for (index, entry) in data.iter().enumerate() {
+            db.insert(format!("key{}", index), entry.clone());
+        }
+        let _ = db.save();
+        let read = new_nabudb("basic_db.xff").unwrap();
+        assert_eq!(data.len(), read.len());
+        for (n, entry) in data.iter().enumerate() {
+            assert_eq!(read.get(&format!("key{}", n)).unwrap(), entry);
+        }
+
+        // cleanup
+        let _ = std::fs::remove_file("basic_db.xff");
+    }
+
+    #[test]
+    fn advanced() {
+        let mut db: NabuDB = new_nabudb("advanced_db.xff").unwrap();
+        let bin_data0 = std::fs::read("src/lib.rs").unwrap();
+        let bin_data1 = std::fs::read("README.md").unwrap();
+        let bin_data2 = std::fs::read("tests/v0.rs").unwrap();
+        let data = vec![
+                XffValue::String("Padding".to_string()),
+                XffValue::Data(Data {len: bin_data0.len(), data: bin_data0}),
+                XffValue::Number(Number::from(42.22222E-222)),
+                XffValue::CommandCharacter(CommandCharacter::UnitSeparator),
+                XffValue::CommandCharacter(CommandCharacter::Space),
+                XffValue::CommandCharacter(CommandCharacter::Delete),
+                XffValue::CommandCharacter(CommandCharacter::NonBreakingSpace),
+                XffValue::CommandCharacter(CommandCharacter::SoftHyphen),
+                XffValue::Number(Number::from(-42.22222E222)),
+                XffValue::String("Padding".to_string()),
+                XffValue::Data(Data {len: bin_data1.len(), data: bin_data1}),
+                XffValue::String("Padding".to_string()),
+                XffValue::Number(Number::from(-42.22222E-222)),
+                XffValue::Number(Number::from(42.22222E+222)),
+                XffValue::String("Padding".to_string()),
+                XffValue::Data(Data {len: bin_data2.len(), data: bin_data2}),
+                XffValue::String("Padding".to_string()),
+                XffValue::Number(Number::from(42.22222E-222)),
+                XffValue::Number(Number::from(-42.22222E+222)),
+                XffValue::String("Padding".to_string()),
+                XffValue::CommandCharacter(CommandCharacter::LineFeed),
+                XffValue::CommandCharacter(CommandCharacter::VerticalTab),
+                XffValue::CommandCharacter(CommandCharacter::FormFeed),
+                XffValue::CommandCharacter(CommandCharacter::CarriageReturn),
+                XffValue::CommandCharacter(CommandCharacter::ShiftOut),
+                XffValue::CommandCharacter(CommandCharacter::ShiftIn),
+                XffValue::CommandCharacter(CommandCharacter::DataLinkEscape),
+                XffValue::CommandCharacter(CommandCharacter::DeviceControl1),
+                XffValue::CommandCharacter(CommandCharacter::DeviceControl2),
+                XffValue::CommandCharacter(CommandCharacter::DeviceControl3),
+                XffValue::CommandCharacter(CommandCharacter::DeviceControl4),
+                XffValue::String("Padding".to_string()),
+                XffValue::CommandCharacter(CommandCharacter::Escape),
+            ];
+        for (index, entry) in data.iter().enumerate() {
+            db.insert(format!("key{}", index), entry.clone());
+        }
+        let _ = db.save();
+        let read = new_nabudb("advanced_db.xff").unwrap();
+        assert_eq!(data.len(), read.len());
+        for (n, entry) in data.iter().enumerate() {
+            assert_eq!(read.get(&format!("key{}", n)).unwrap(), entry);
+        }
+
+        // cleanup
+        let _ = std::fs::remove_file("advanced_db.xff");
+    }
+
+    #[test]
+    fn complex() {
+        let mut db: NabuDB = new_nabudb("complex_db.xff").unwrap();
+        let bin_data0 = std::fs::read("pictures/xff-char-chart.jpeg").unwrap();
+        let bin_data1 = std::fs::read("pictures/xff-cmd-char-chart.jpeg").unwrap();
+        let bin_data2 = std::fs::read("pictures/xff-main-chart.jpeg").unwrap();
+        let bin_data3 = std::fs::read("LICENSE").unwrap();
+        let bin_data5 = std::fs::read("README.md").unwrap();
+        let bin_data6 = std::fs::read("tests/v0.rs").unwrap();
+        let data = vec![
+                XffValue::String("Padding".to_string()),
+                XffValue::Data(Data {len: bin_data0.len(), data: bin_data0}),
+                XffValue::Number(Number::from(42.22222E-222)),
+                XffValue::CommandCharacter(CommandCharacter::UnitSeparator),
+                XffValue::Data(Data {len: bin_data6.len(), data: bin_data6}),
+                XffValue::CommandCharacter(CommandCharacter::SoftHyphen),
+                XffValue::Number(Number::from(-42.22222E222)),
+                XffValue::String("Padding".to_string()),
+                XffValue::Data(Data::from(bin_data1)),
+                XffValue::String("Padding".to_string()),
+                XffValue::Number(Number::from(42.22222E+222)),
+                XffValue::String("Padding".to_string()),
+                XffValue::Data(Data::from(bin_data2)),
+                XffValue::String("Padding".to_string()),
+                XffValue::Number(Number::from(42.22222E-222)),
+                XffValue::String("Padding".to_string()),
+                XffValue::CommandCharacter(CommandCharacter::LineFeed),
+                XffValue::CommandCharacter(CommandCharacter::DeviceControl4),
+                XffValue::String("Padding".to_string()),
+                XffValue::CommandCharacter(CommandCharacter::Escape),
+                XffValue::Data(Data::from(bin_data3)),
+                XffValue::Data(Data::from(bin_data5)),
+            ];
+        for (index, entry) in data.iter().enumerate() {
+            db.insert(format!("key{}", index), entry.clone());
+        }
+        let _ = db.save();
+        let read = new_nabudb("complex_db.xff").unwrap();
+        assert_eq!(data.len(), read.len());
+        for (n, entry) in data.iter().enumerate() {
+            assert_eq!(read.get(&format!("key{}", n)).unwrap(), entry);
+        }
+
+        // cleanup
+        let _ = std::fs::remove_file("complex_db.xff");
     }
 }
