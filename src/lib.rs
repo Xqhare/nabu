68a99818d2a101a93a009831a91605332c6dcdaf
Xqhare <xqhare@web.de>
2024-07-27 23:49:18 +0200

    doc: working on doc in lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+/*!
+##### Key Value Core Usage
+```rust
+use nabu::features::key_value::core::{read_core, write_core};
+use nabu::xff::value::{XffValue, Number, Data};
+use std::{collections::BTreeMap, path::Path};
+
+# #[cfg(feature = "key_value_core")]
+# fn main() {
+let mut map: BTreeMap<String, XffValue> = Default::default();
+map.insert("key0".to_string(), XffValue::String("Hello Mom!".to_string()));
+map.insert("key1".to_string(), XffValue::Number(Number::from(-42)));
+map.insert("key2".to_string(), XffValue::Data(Data::from(vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])));
+let write = write_core(&Path::new("xff-example-data/xff-core-example-file.xff"), map.clone());
+assert!(write.is_ok());
+let read = read_core(&Path::new("xff-example-data/xff-core-example-file.xff"));
+assert!(read.is_ok());
+# }
+```
+*/
+
 mod error;
 
 pub mod xff;
@@ -8,12 +29,11 @@ const XFF_VERSION: u8 = 0;
 /// 
 /// # Example
 /// ```rust
-/// use std::fs::remove_file;
-/// use nabu::serde::{read, write};
+/// use nabu::serde::{read, write, remove_file};
 /// use nabu::xff::value::{CommandCharacter, Data, Number, XffValue};
 /// // No matter what the extension of the path you provide, it will be converted to .xff
-/// let path = "xff-example-data/v0.txt";
-/// let path_2 = "xff-example-data/v0.xff";
+/// let path = "xff-example-data/serde-example.txt";
+/// let path_2 = "xff-example-data/serde-example.xff";
 /// let data = {
 ///     vec![
 ///         XffValue::String("hello mom".to_string()),
@@ -25,7 +45,7 @@ const XFF_VERSION: u8 = 0;
 /// assert!(tmp_2.is_ok());
 /// let ok = tmp_2.unwrap();
 /// assert_eq!(ok[0], data[0]);
-/// // delete file
+/// // delete file with the inbuilt remove_file function
 /// remove_file(path_2).unwrap();
 /// ```
 pub mod serde {
@@ -46,9 +66,15 @@ pub mod serde {
     /// # Example
     /// ```rust
     /// use nabu::serde::read;
+    /// use nabu::xff::value::XffValue;
     ///
     /// let tmp = read("xff-example-data/v0.xff");
     /// assert!(tmp.is_ok());
+    /// let data: Vec<XffValue> = tmp.unwrap();
+    /// for value in data {
+    ///     // do something with the value
+    ///     println!("{:?}", value);
+    /// }
     /// ```
     pub fn read<P>(path: P) -> Result<Vec<XffValue>, NabuError> where P: AsRef<std::path::Path> {
         let path_with_xff_extension = path.as_ref().with_extension("xff");
@@ -110,8 +136,8 @@ pub mod serde {
         serialize_xff(&path_with_xff_extension, data, xff_version)
     }
 
-    /// A convenience function to remove an XFF file
-    /// Deletes a file from disk, take care!
+    /// A convenience function to delete any XFF file from disk
+    /// The file will be gone, take care!
     ///
     /// # Arguments
     /// * `path` - The path to the file to remove
@@ -133,12 +159,12 @@ pub mod serde {
     /// let _ = write("xff-example-data/remove.xff", data.clone());
     ///
     /// // Now we can remove it
-    /// use nabu::serde::remove;
+    /// use nabu::serde::remove_file;
     ///
-    /// let tmp = remove("xff-example-data/remove.xff");
+    /// let tmp = remove_file("xff-example-data/remove.xff");
     /// assert!(tmp.is_ok());
     /// ```
-    pub fn remove<P>(path: P) -> Result<(), NabuError> where P: AsRef<std::path::Path> {
+    pub fn remove_file<P>(path: P) -> Result<(), NabuError> where P: AsRef<std::path::Path> {
         let path_with_xff_extension = path.as_ref().with_extension("xff");
         Ok(std::fs::remove_file(path_with_xff_extension)?)
     }
@@ -150,7 +176,18 @@ pub mod serde {
 pub mod features;
 
 #[cfg(feature = "key_value_core")]
-/// Module to read and write a basic key-value store
+/// Module to read and write a basic key-value store in the form of a `BTreeMap`
+///
+/// # Example
+/// ```rust
+/// use std::{collections::BTreeMap, path::Path};
+/// use nabu::features::key_value::core::read_core;
+/// use nabu::xff::value::XffValue;
+///
+/// let data = read_core(&Path::new("xff-example-data/key_value_core.xff"));
+/// assert!(data.is_ok());
+/// let map: BTreeMap<String, XffValue> = data.unwrap();
+/// ```
 pub mod key_value_core {
     use std::collections::BTreeMap;
 
@@ -158,18 +195,59 @@ pub mod key_value_core {
 
     /// Reads the content of a XFF file and returns a BTreeMap
     /// Please note that only XFF files written by the `write` function of this module are supported
+    ///
+    /// # Arguments
+    /// * `path` - The path to the file to read
+    ///
+    /// # Example
+    /// ```rust
+    /// use nabu::features::key_value::core::read_core;
+    /// use nabu::xff::value::XffValue;
+    /// use std::collections::BTreeMap;
+    /// use std::path::Path;
+    ///
+    /// let data = read_core(&Path::new("xff-example-data/key_value_core.xff"));
+    /// assert!(data.is_ok());
+    /// let map: BTreeMap<String, XffValue> = data.unwrap();
+    /// ```
     pub fn read<P>(path: P) -> Result<BTreeMap<String, XffValue>, NabuError> where P: AsRef<std::path::Path>{
         let path_with_xff_extension = path.as_ref().with_extension("xff");
         read_core(&path_with_xff_extension)
     }
 
     /// Writes a BTreeMap to a XFF file
+    ///
+    /// # Arguments
+    /// * `path` - The path to the file to write
+    /// * `data` - The BTreeMap to write
+    ///
+    /// # Example
+    /// ```rust
+    /// use nabu::key_value_core::{write, new_core_store};
+    /// use nabu::xff::value::{XffValue, Number};
+    ///
+    /// let mut data = new_core_store();
+    /// data.insert("key0".to_string(), XffValue::String("value0".to_string()));
+    /// data.insert("key1".to_string(), XffValue::Number(Number::from(42)));
+    ///
+    /// let tmp = write("xff-example-data/key_value_core.xff", data.clone());
+    /// assert!(tmp.is_ok());
+    /// ```
     pub fn write<P>(path: P, data: BTreeMap<String, XffValue>) -> Result<(), NabuError> where P: AsRef<std::path::Path> {
         let path_with_xff_extension = path.as_ref().with_extension("xff");
         write_core(&path_with_xff_extension, data)
     }
 
     /// Creates a new BTreeMap
+    ///
+    /// # Example
+    /// ```rust
+    /// use std::collections::BTreeMap;
+    /// use nabu::{key_value_core::new_core_store, xff::value::XffValue};
+    ///
+    /// let data: BTreeMap<String, XffValue> = new_core_store();
+    /// assert!(data.is_empty());
+    /// ```
     pub fn new_core_store() -> BTreeMap<String, XffValue> {
         BTreeMap::new()
     }
@@ -191,10 +269,11 @@ pub mod key_value_store {
     /// # Example
     /// ```rust
     /// use nabu::key_value_store::new_nabudb;
+    /// use nabu::features::key_value::store::NabuDB;
     /// use nabu::xff::value::{XffValue, CommandCharacter, Data, Number};
     /// 
     /// let path = "xff-example-data/nabuDB_main_example.xff";
-    /// let mut db = new_nabudb(path).unwrap();
+    /// let mut db: NabuDB = new_nabudb(path).unwrap();
     /// db.insert("key0".to_string(), XffValue::String("value0".to_string()));
     /// db.insert("key1".to_string(), XffValue::Number(Number::from(-42)));
     /// db.insert("key2".to_string(), XffValue::CommandCharacter(CommandCharacter::LineFeed));
