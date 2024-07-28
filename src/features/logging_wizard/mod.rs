7db06ef1d19276ca6afd0bc7fa41ef79de5a4b77
Xqhare <xqhare@web.de>
2024-07-29 01:56:43 +0200

    feat: created basic api and deserializer

--- src/features/logging_wizard/mod.rs
+++ src/features/logging_wizard/mod.rs
@@ -1,5 +1,108 @@
 mod serde;
 
-pub fn cast_log(log_data: BTreeMap<String, >) -> Result<Vec<XffValue>, NabuError> {
-    
+use crate::{xff::value::{XffValue, CommandCharacter}, features::logging_wizard::serde::read_log_wizard, error::NabuError};
+
+use std::collections::BTreeMap;
+
+/// Module to create, read and write XFF-Logging files
+///
+/// # Creating a LoggingWizard
+/// There are several ways to create a LoggingWizard, depending on your need.
+///
+/// Should you want to load all previous logs, you can use the `LoggingWizard::from_file` function.
+///
+/// ## Example
+/// ```rust
+/// use nabu::features::logging_wizard::LoggingWizard;
+/// use nabu::xff::value::XffValue;
+///
+/// let mut wizard = LoggingWizard::from_file("xff-example-data/logging_wizard.xff");
+/// assert!(wizard.is_ok());
+/// ```
+///
+/// If you want to create a new LoggingWizard or want to add logs to an existing one, you can use the `LoggingWizard::new` function.
+/// Here the file would not be decoded, but loaded into memory and all new logs would be appended
+/// to the end.
+/// If the supplied file does not exist, it would be created.
+///
+/// ## Example
+/// ```rust
+/// use nabu::features::logging_wizard::LoggingWizard;
+/// use nabu::xff::value::XffValue;
+///
+/// let mut wizard = LoggingWizard::new("xff-example-data/logging_wizard.xff");
+/// assert!(wizard.is_ok());
+/// ```
+///
+/// You can split up the logs into multiple files by calling the `LoggingWiard::new` function with
+/// a different path if you so desire, however the LoggingWiard is designed to work with a single
+/// file, think of it more like a single .log file.
+pub struct LoggingWizard {
+    /// This stores the path of the file where the logs are written, this file would only be
+    /// crated if not existent, otherwise a simple append would be used
+    path: std::path::PathBuf,
+    /// This stores all the logs in chronological order
+    pub logs: Vec<Log>,
 }
+
+impl LoggingWizard {
+    /// Creates a new LoggingWizard from a file
+    ///
+    /// # Arguments
+    /// * `path` - The path to the file to read
+    ///
+    /// # Example
+    /// ```rust
+    /// use std::path::Path;
+    /// use nabu::features::logging_wizard::LoggingWizard;
+    ///
+    /// let path = Path::new("xff-example-data/logging_wizard.xff");
+    /// let wizard = LoggingWizard::from_file(path);
+    /// assert!(wizard.is_ok());
+    /// ```
+    pub fn from_file<P>(path: P) -> Result<LoggingWizard, NabuError> where P: AsRef<std::path::Path> {
+        let data = read_log_wizard(path)?;
+        todo!()
+    }
+
+    /// Creates a new LoggingWizard
+    ///
+    /// # Arguments
+    /// * `path` - The path to the file to write
+    ///
+    /// # Example
+    /// ```rust
+    /// use std::path::Path;
+    /// use nabu::features::logging_wizard::LoggingWizard;
+    ///
+    /// let path = Path::new("xff-example-data/logging_wizard.xff");
+    /// let wizard = LoggingWizard::new(path);
+    /// assert!(wizard.is_ok());
+    /// ```
+    pub fn new<P>(path: P) -> Result<LoggingWizard, NabuError> where P: AsRef<std::path::Path> {
+        let path = path.as_ref().to_path_buf();
+        todo!()
+    }
+}
+
+/// Stores all data of a single log. This can be as much data as you want.
+pub struct Log {
+    /// Every data point of the log is stored here
+    pub log_data: Vec<LogData>,
+}
+
+/// Stores a single data point of the log
+pub struct LogData {
+    /// The name of the data point
+    pub name: String,
+    /// The value of the data point
+    pub value: XffValue,
+    /// The optional metadata of the data point
+    /// E.g. some kind of picture is saved inside the value, then you could store the extension
+    /// here in the form of `("extension", "jpg")`
+    ///
+    /// There is no limit on the number of metadata entries, but they have to be ASCII strings
+    pub optional_metadata: BTreeMap<String, String>,
+}
+
+
--- src/features/logging_wizard/serde.rs
+++ src/features/logging_wizard/serde.rs
@@ -1,5 +1,104 @@
-use crate::{serde::{read, write}, xff::value::XffValue, error::NabuError};
+use std::collections::BTreeMap;
+use crate::{serde::{read, write}, xff::value::{XffValue, CommandCharacter}, error::NabuError, features::logging_wizard::{Log, LogData}};
 
-pub fn read_log_file<P>(path: P) -> Result<Vec<String>, NabuError> where P: AsRef<std::path::Path> {
-    
+use super::LoggingWizard;
+
+impl LoggingWizard {
+
+    pub fn write_log_wizard(&self) -> Result<(), NabuError> {
+        todo!()
+    }
+
+    fn append(&self) -> Result<(), NabuError> {
+        let mut file_as_bytes = std::fs::read(self.path.clone())?;
+        // Dropping the last byte, EM byte
+        let _ = file_as_bytes[file_as_bytes.len() - 1];
+        let data = self.to_bytes();
+        // appending data to file
+        file_as_bytes.extend(data);
+        std::fs::write(self.path.clone(), file_as_bytes)?;
+        Ok(())
+    }
+
+    fn to_bytes(&self) -> Vec<u8> {
+        todo!()
+    }
+}
+
+pub fn read_log_wizard<P>(path: P) -> Result<LoggingWizard, NabuError> where P: AsRef<std::path::Path> {
+    let mut data = read(path.as_ref())?;
+    let mut logs: Vec<Log> = Vec::new();
+    while data.len() > 0 {
+        let current_entry: XffValue = data.remove(0);
+        match current_entry {
+            XffValue::CommandCharacter(CommandCharacter::FileSeparator) => {
+                // Logdata starts here
+                let mut log_data: Vec<LogData> = Vec::new();
+                let next_entry: XffValue = data.remove(0);
+                match next_entry {
+                    XffValue::CommandCharacter(CommandCharacter::GroupSeparator) => {
+                        while data[0] != XffValue::CommandCharacter(CommandCharacter::GroupSeparator) {
+                            let name = data.remove(0);
+                            let value = data.remove(0);
+                            let optional_metadata: BTreeMap<String, String> = {
+                                let mut out: BTreeMap<String, String> = BTreeMap::new();
+                                if data[0] == XffValue::CommandCharacter(CommandCharacter::RecordSeparator) {
+                                    let _ = data.remove(0);
+                                    while data[0] != XffValue::CommandCharacter(CommandCharacter::RecordSeparator) {
+                                        if data[0] == XffValue::CommandCharacter(CommandCharacter::UnitSeparator) {
+                                            let _ = data.remove(0);
+                                            let key = data.remove(0).as_string();
+                                            let value = data.remove(0).as_string();
+                                            if key.is_none() || value.is_none() {
+                                                Err(NabuError::InvalidXFF(format!("Invalid XFF, expected a String key-value pair got KEY: {:?}, VALUE {:?}", key, value)))?
+                                            } else {
+                                                out.insert(key.unwrap(), value.unwrap());
+                                                let exit_marker = data.remove(0);
+                                                if exit_marker != XffValue::CommandCharacter(CommandCharacter::UnitSeparator) {
+                                                    Err(NabuError::InvalidXFF(format!("Invalid XFF, expected UnitSeparator got {:?}", exit_marker)))?
+                                                }
+                                            }
+                                        } else {
+                                            Err(NabuError::InvalidXFF(format!("Invalid XFF, expected UnitSeparator got {:?}", data[0])))?
+                                        }
+                                    }
+                                    // remove the trailing RecordSeparator
+                                    let trailing = data.remove(0);
+                                    if trailing != XffValue::CommandCharacter(CommandCharacter::RecordSeparator) {
+                                        Err(NabuError::InvalidXFF(format!("Invalid XFF, expected RecordSeparator got {:?}", trailing)))?
+                                    }
+                                    out
+                                } else {
+                                    BTreeMap::new()
+                                }
+                            };
+                            if data[0] != XffValue::CommandCharacter(CommandCharacter::GroupSeparator) {
+                                Err(NabuError::InvalidXFF(format!("Invalid XFF, expected GroupSeparator got {:?}", data[0])))?
+                            } else {
+                                log_data.push(LogData {
+                                name: name.as_string().unwrap(),
+                                value,
+                                optional_metadata
+                                });
+                                let _ = data.remove(0);
+                            }
+                        }
+                        if data[0] != XffValue::CommandCharacter(CommandCharacter::GroupSeparator) {
+                            Err(NabuError::InvalidXFF(format!("Invalid XFF, expected FileSeparator got {:?}", data[0])))?
+                        } else {
+                            logs.push(Log { log_data });
+                            let _ = data.remove(0);
+                        }
+                    }
+                    _ => {
+                        Err(NabuError::InvalidXFF(format!("Invalid XFF, expected GroupSeparator got {:?}", next_entry)))?
+                    }
+                }
+            }
+            _ => {
+                Err(NabuError::InvalidXFF(format!("Invalid XFF, expected FileSeparator got {:?}", current_entry)))?
+            }
+        }
+    }
+    Ok(LoggingWizard { logs, path: path.as_ref().to_path_buf() })
 }
