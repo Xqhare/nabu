b31ec05d49746c0a476dbd1f3f96b5c9142b33e1
Xqhare <xqhare@web.de>
2024-08-03 22:00:39 +0200

    chore: renamed test and file

--- tests/key_value.rs
+++ tests/key_value.rs
@@ -1,16 +1,16 @@
 #[cfg(test)]
 #[cfg(feature = "logging_wizard")]
-mod tests {
+mod logging_wizard {
     use nabu::{logging_wizard::{LoggingWizard, Log, LogData}, xff::value::{CommandCharacter, Data, Number, XffValue}};
     #[test]
-    fn test_name() {
-        let mut wizard = LoggingWizard::new("test_name.xff");
+    fn logging_test_name() {
+        let mut wizard = LoggingWizard::new("logging_test_name.xff");
         let mut log = Log::new();
         log.add_log_data(LogData::new("name", XffValue::String("value".to_string()), None));
         wizard.add_log(log);
         let out = wizard.save();
         assert!(out.is_ok());
-        let read_wizard = LoggingWizard::from_file("test_name.xff").unwrap();
+        let read_wizard = LoggingWizard::from_file("logging_test_name.xff").unwrap();
         assert_eq!(read_wizard.logs.len(), 1);
         assert_eq!(read_wizard.logs[0].log_data.len(), 1);
         assert_eq!(read_wizard.logs[0].log_data[0].name, "name");
@@ -18,8 +18,6 @@ mod tests {
         assert_eq!(read_wizard.logs[0].log_data[0].optional_metadata.is_empty(), true);
     }
 }
-
-
 
 #[cfg(test)]
 #[cfg(feature = "key_value_core")]
