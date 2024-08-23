a30c2f3f93da91604fe20c2f21ee57f17e5a1869
Xqhare <xqhare@web.de>
2024-08-23 14:13:07 +0200

    feat: restructuring

--- src/xff/serializer/mod.rs
+++ src/xff/serializer/mod.rs
@@ -9,7 +9,8 @@ use super::value::XffValue;
 /// Determines the XFF version to use and calls the appropriate serializer
 ///
 /// Because of version 0, the data argument has to be a vector, even if only one XffElement is
-/// permissable as with version 1 
+/// permissable as with version 1. In this case just "wrap" it in a vector, only the first element
+/// is used.
 ///
 /// # Arguments
 /// * `path` - The path to the file to write
@@ -55,42 +56,75 @@ fn serialize_xff_v1_value(data: &XffValue) -> Result<Vec<u8>> {
             }
             // now byte structure and push
             out.push(1);
-            for entry in encode_length(tmp_str.len()) {
-                out.push(entry);
-            }
-            for entry in tmp_str {
-                out.push(entry);
-            }
+            out.extend(encode_length(tmp_str.len()));
+            out.extend(tmp_str);
             out.push(1);
         }
         XffValue::Number(n) => {
             // first create the string from the number
             let mut tmp_str: Vec<u8> = Default::default();
             let tmp = n.as_string();
-            for entry in tmp.into_bytes() {
-                tmp_str.push(entry);
+            for entry in tmp.chars() {
+                tmp_str.push(entry as u8);
             }
             // now byte structure and push
             out.push(2);
-            for entry in encode_length(tmp_str.len()) {
-                out.push(entry);
-            }
-            for entry in tmp_str {
-                out.push(entry);
-            }
+            out.extend(encode_length(tmp_str.len()));
+            out.extend(tmp_str);
             out.push(2);
         }
         XffValue::Array(a) => {
-            
+            // create the array
+            let mut array_bytes: Vec<u8> = Default::default();
+            for value in a.iter() {
+                array_bytes.extend(serialize_xff_v1_value(value)?);
+                // RS separator
+                array_bytes.push(30)
+            }
+            // byte structure and push
+            out.push(3);
+            out.extend(encode_length(array_bytes.len()));
+            out.extend(array_bytes);
+            out.push(3);
         }
         XffValue::Object(o) => {
-            
+            // create the object
+            let mut object_bytes: Vec<u8> = Default::default();
+            for (key, value) in o.iter() {
+                // GS
+                object_bytes.push(29);
+                // key
+                object_bytes.extend(serialize_xff_v1_value(&XffValue::String(key.clone()))?);
+                // US
+                object_bytes.push(31);
+                // value
+                out.extend(serialize_xff_v1_value(value)?);
+                // Trailing GS
+                object_bytes.push(29);
+                // RS separator
+                object_bytes.push(30)
+            }
+            // byte structure and push
+            out.push(4);
+            out.extend(encode_length(object_bytes.len()));
+            out.extend(object_bytes);
+            out.push(4);
         }
         XffValue::Data(d) => {
+            out.push(5);
+            out.extend(encode_length(d.len));
+            out.extend(d.data.clone());
+            out.push(5);
         }
         XffValue::Boolean(b) => {
+            if *b {
+                out.push(16);
+            } else {
+                out.push(17);
+            }
         }
         XffValue::Null => {
+            out.push(0);
         }
         _ => Err(NabuError::InvalidXFFVersion(data.clone(), 1))?
     }
@@ -99,52 +133,36 @@ fn serialize_xff_v1_value(data: &XffValue) -> Result<Vec<u8>> {
 
 fn encode_length(len: usize) -> Vec<u8> {
     if len <= 255 {
-        let mut out: Vec<u8> = vec![1];
-        for entry in len.to_le_bytes().to_vec() {
-            out.push(entry);
-        }
+        let mut out: Vec<u8> = u8::from(1).to_le_bytes().to_vec();
+        out.extend(len.to_le_bytes().to_vec());
         return out;
     } if len <= 65_535 {
-        let mut out: Vec<u8> = vec![2];
-        for entry in len.to_le_bytes().to_vec() {
-            out.push(entry);
-        }
+        let mut out: Vec<u8> = u8::from(2).to_le_bytes().to_vec();
+        out.extend(len.to_le_bytes().to_vec());
         return out;
     } if len <= 16_777_215 {
-        let mut out: Vec<u8> = vec![3];
-        for entry in len.to_le_bytes().to_vec() {
-            out.push(entry);
-        }
+        let mut out: Vec<u8> = u8::from(3).to_le_bytes().to_vec();
+        out.extend(len.to_le_bytes().to_vec());
         return out;
     } if len <= 4_294_967_295 {
-        let mut out: Vec<u8> = vec![4];
-        for entry in len.to_le_bytes().to_vec() {
-            out.push(entry);
-        }
+        let mut out: Vec<u8> = u8::from(4).to_le_bytes().to_vec();
+        out.extend(len.to_le_bytes().to_vec());
         return out;
     } if len <= 1_099_511_627_775 {
-        let mut out: Vec<u8> = vec![5];
-        for entry in len.to_le_bytes().to_vec() {
-            out.push(entry);
-        }
+        let mut out: Vec<u8> = u8::from(5).to_le_bytes().to_vec();
+        out.extend(len.to_le_bytes().to_vec());
         return out;
     } if len <= 281_474_976_710_655 {
-        let mut out: Vec<u8> = vec![6];
-        for entry in len.to_le_bytes().to_vec() {
-            out.push(entry);
-        }
+        let mut out: Vec<u8> = u8::from(6).to_le_bytes().to_vec();
+        out.extend(len.to_le_bytes().to_vec());
         return out;
-    } else if len <= 72_057_594_037_927_935 {
-        let mut out: Vec<u8> = vec![7];
-        for entry in len.to_le_bytes().to_vec() {
-            out.push(entry);
-        }
+    } else if len <= 72_057_594_037_927_35 {
+        let mut out: Vec<u8> = u8::from(7).to_le_bytes().to_vec();
+        out.extend(len.to_le_bytes().to_vec());
         return out;
     } else {
-        let mut out: Vec<u8> = vec![8];
-        for entry in len.to_le_bytes().to_vec() {
-            out.push(entry);
-        }
+        let mut out: Vec<u8> = u8::from(8).to_le_bytes().to_vec();
+        out.extend(len.to_le_bytes().to_vec());
         return out;
     }
 }
