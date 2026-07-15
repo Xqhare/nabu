4589d3f705f19728abd203f4d90066a36714ce6c
Xqhare <xqhare@web.de>
2026-07-15 03:00:56 +0200

    add(bench): implement full v4 benchmark suite cases

--- benches/bench_v4.rs
+++ benches/bench_v4.rs
@@ -1,6 +1,6 @@
 use std::hint::black_box;
 use std::time::{Duration, Instant};
-use nabu::{XffValue, Number, Array, Object, Data, Graph};
+use nabu::{XffValue, Array, Object, Data, Graph};
 use nabu::serde::{serialize_xff_to_buffer, deserialize_xff_from_buffer};
 
 struct BenchResult {
@@ -89,7 +89,7 @@ fn main() {
     let val_primitives = XffValue::Array(Array::from(primitives));
     let buf_primitives = serialize_xff_to_buffer(vec![val_primitives.clone()], 4).expect("Failed serialization");
 
-    let bench_ser_primitives = run_bench(
+    run_bench(
         "primitives",
         "Ser",
         50000,
@@ -97,10 +97,9 @@ fn main() {
             serialize_xff_to_buffer(vec![val_primitives.clone()], 4).unwrap()
         },
         None
-    );
-    bench_ser_primitives.print();
+    ).print();
 
-    let bench_deser_primitives = run_bench(
+    run_bench(
         "primitives",
         "Deser",
         50000,
@@ -108,6 +107,118 @@ fn main() {
             deserialize_xff_from_buffer(&buf_primitives).unwrap()
         },
         None
-    );
-    bench_deser_primitives.print();
+    ).print();
+
+    // Case 2: Large Array (1000 integers)
+    let large_arr_values: Vec<XffValue> = (0..1000).map(|i| XffValue::from(i)).collect();
+    let val_large_arr = XffValue::Array(Array::from(large_arr_values));
+    let buf_large_arr = serialize_xff_to_buffer(vec![val_large_arr.clone()], 4).expect("Failed serialization");
+
+    run_bench(
+        "large_array",
+        "Ser",
+        1000,
+        || {
+            serialize_xff_to_buffer(vec![val_large_arr.clone()], 4).unwrap()
+        },
+        Some(buf_large_arr.len())
+    ).print();
+
+    run_bench(
+        "large_array",
+        "Deser",
+        1000,
+        || {
+            deserialize_xff_from_buffer(&buf_large_arr).unwrap()
+        },
+        Some(buf_large_arr.len())
+    ).print();
+
+    // Case 3: Large Object (100 key-value pairs)
+    let mut large_obj = Object::new();
+    for i in 0..100 {
+        large_obj.insert(format!("key_{}", i), XffValue::from(i));
+    }
+    let val_large_obj = XffValue::Object(large_obj);
+    let buf_large_obj = serialize_xff_to_buffer(vec![val_large_obj.clone()], 4).expect("Failed serialization");
+
+    run_bench(
+        "large_object",
+        "Ser",
+        1000,
+        || {
+            serialize_xff_to_buffer(vec![val_large_obj.clone()], 4).unwrap()
+        },
+        Some(buf_large_obj.len())
+    ).print();
+
+    run_bench(
+        "large_object",
+        "Deser",
+        1000,
+        || {
+            deserialize_xff_from_buffer(&buf_large_obj).unwrap()
+        },
+        Some(buf_large_obj.len())
+    ).print();
+
+    // Case 4: Large Data (1 MB binary payload)
+    let raw_data = vec![127u8; 1024 * 1024];
+    let val_large_data = XffValue::Data(Data::from(raw_data));
+    let buf_large_data = serialize_xff_to_buffer(vec![val_large_data.clone()], 4).expect("Failed serialization");
+
+    run_bench(
+        "large_data (1 MB)",
+        "Ser",
+        100,
+        || {
+            serialize_xff_to_buffer(vec![val_large_data.clone()], 4).unwrap()
+        },
+        Some(buf_large_data.len())
+    ).print();
+
+    run_bench(
+        "large_data (1 MB)",
+        "Deser",
+        100,
+        || {
+            deserialize_xff_from_buffer(&buf_large_data).unwrap()
+        },
+        Some(buf_large_data.len())
+    ).print();
+
+    // Case 5: Graph (50 nodes, 100 connections)
+    let mut graph = Graph::new();
+    let nodes: Vec<_> = (0..50)
+        .map(|i| graph.add_node(XffValue::from(format!("node_{}", i)), XffValue::Null))
+        .collect();
+    for i in 0..50 {
+        let from = nodes[i];
+        let to = nodes[(i + 1) % 50];
+        let to_other = nodes[(i + 25) % 50];
+        let _ = graph.add_connection(from, to, XffValue::from(format!("edge_{}_to_{}", i, (i + 1) % 50)));
+        let _ = graph.add_connection(from, to_other, XffValue::from(format!("edge_{}_to_{}", i, (i + 25) % 50)));
+    }
+    let val_graph = XffValue::Graph(graph);
+    let buf_graph = serialize_xff_to_buffer(vec![val_graph.clone()], 4).expect("Failed serialization");
+
+    run_bench(
+        "graph",
+        "Ser",
+        500,
+        || {
+            serialize_xff_to_buffer(vec![val_graph.clone()], 4).unwrap()
+        },
+        Some(buf_graph.len())
+    ).print();
+
+    run_bench(
+        "graph",
+        "Deser",
+        500,
+        || {
+            deserialize_xff_from_buffer(&buf_graph).unwrap()
+        },
+        Some(buf_graph.len())
+    ).print();
 }
